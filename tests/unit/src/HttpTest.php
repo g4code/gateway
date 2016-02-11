347f2887926fe3fc24e0b2edf163fd2f151c8b0e
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-02-11 15:02:34 +0100

    Http Client class, facade for Zend Http Client

--- tests/unit/src/HttpTest.php
+++ tests/unit/src/HttpTest.php
@@ -5,17 +5,26 @@ use G4\Gateway\Http;
 class HttpTest extends \PHPUnit_Framework_TestCase
 {
 
+    private $http;
+
+    private $optionsMock;
+
     protected function setUp()
     {
-
+//        $this->optionsMock = $this->getMockBuilder('\G4\Gateway\Options')
+//            ->disableOriginalConstructor()
+//            ->getMock();
+//
+//        $this->http = new Http($this->optionsMock);
     }
 
     protected function tearDown()
     {
-
+        $this->optionsMock = null;
+        $this->http = null;
     }
 
-    public function test()
+    public function testDelete()
     {
         
     }
