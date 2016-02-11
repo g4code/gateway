2847d26345b9672bd553038d3e240837cec0a93e
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-02-11 12:22:10 +0100

    Unit tests for Options

--- tests/unit/src/OptionsTest.php
+++ tests/unit/src/OptionsTest.php
@@ -5,6 +5,9 @@ use G4\Gateway\Options;
 class OptionsTest extends \PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var Options
+     */
     private $options;
 
     protected function setUp()
@@ -19,6 +22,50 @@ class OptionsTest extends \PHPUnit_Framework_TestCase
 
     public function testHeaders()
     {
+        $data = [
+            'Accept' => 'application/json',
+        ];
 
+        $this->options->setHeaders($data);
+
+        $this->assertEquals($data, $this->options->getHeaders());
+    }
+
+    public function testUri()
+    {
+        $data = 'http://google.com';
+
+        $this->options->setUri($data);
+
+        $this->assertEquals($data, $this->options->getUri());
+    }
+
+    public function testResourceKey()
+    {
+        $data = 'resource';
+
+        $this->options->setResourceKey($data);
+
+        $this->assertEquals($data, $this->options->getResourceKey());
+    }
+
+    public function testSslVerifyPeer()
+    {
+        $this->assertNull($this->options->getSslVerifyPeer());
+
+        $this->options->setSslVerifyPeer(true);
+
+        $this->assertTrue($this->options->getSslVerifyPeer());
+    }
+
+    public function testTimeout()
+    {
+        $this->assertEquals(Options::DEFAULT_TIMEOUT, $this->options->getTimeout());
+
+        $data = 30;
+
+        $this->options->setTimeout($data);
+
+        $this->assertEquals($data, $this->options->getTimeout());
     }
 }
