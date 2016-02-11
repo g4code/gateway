463be4a555e68fe8b54ce5bc7dbbbbad16432149
Drasko Gomboc <drasko.gomboc@gmail.com>
2016-02-11 11:18:34 +0100

    Added Options and Http tests case classes

---
+++ test/src/HttpTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use G4\Gateway\Http;
+
+class HttpTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+
+    }
+
+    protected function tearDown()
+    {
+
+    }
+
+    public function test()
+    {
+        
+    }
+}
---
+++ test/src/OptionsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use G4\Gateway\Options;
+
+class OptionsTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $options;
+
+    protected function setUp()
+    {
+        $this->options = new Options();
+    }
+
+    protected function tearDown()
+    {
+        $this->options = null;
+    }
+
+    public function test()
+    {
+
+    }
+}
