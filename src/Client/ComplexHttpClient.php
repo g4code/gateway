f8a64c4d09a6e2274d89060d76c35c14be22f48f
Sinisa Lackovic <sinisalackovic@yahoo.com>
2017-06-27 12:54:09 +0200

    Separated a logic for sending curl requests over the Zend curl to a new class, complex http client

---
+++ src/Client/ComplexHttpClient.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace G4\Gateway\Client;
+
+use G4\Gateway\Url;
+use G4\Gateway\Options;
+use G4\Gateway\Response;
+use G4\Gateway\HttpMethod;
+use G4\Gateway\Profiler\Ticker;
+
+class ComplexHttpClient implements HttpClientInterface
+{
+    /**
+     * @var Options
+     */
+    private $options;
+
+    /**
+     * @var Ticker
+     */
+    private $profiler;
+
+    /**
+     * ComplexHttpClient constructor.
+     * @param Options $options
+     */
+    public function __construct(Options $options)
+    {
+        $this->options = $options;
+    }
+
+    /**
+     * @return \Zend\Http\Client
+     */
+    public function getClient()
+    {
+        $client = new \Zend\Http\Client();
+        $client
+            ->setAdapter('\Zend\Http\Client\Adapter\Curl')
+            ->setEncType(\Zend\Http\Client::ENC_URLENCODED)
+            ->setOptions([
+                'timeout' => $this->options->getTimeout(),
+                'curloptions' => [
+                    CURLOPT_SSL_VERIFYPEER => $this->options->getSslVerifyPeer()
+                ],
+            ]);
+
+        $client->getRequest()->getHeaders()->addHeaders($this->options->getHeaders());
+
+        return $client;
+    }
+
+    /**
+     * @return Ticker
+     */
+    public function getProfiler()
+    {
+        if (! $this->profiler instanceof Ticker) {
+            $this->profiler = Ticker::getInstance();
+        }
+        return $this->profiler;
+    }
+
+    /**
+     * @param Url $url
+     * @param HttpMethod $method
+     * @return Response
+     */
+    public function send(Url $url, HttpMethod $method)
+    {
+        $uniqueId   = $this->getProfiler()->start();
+        $uri        = $url->getUri();
+        $params     = $url->getParams()->toArray();
+        $httpMethod = (string) $method;
+
+        $method->isPost()
+            ? $this->getClient()->setParameterPost($params)
+            : $this->getClient()->setParameterGet($params);
+
+        $this->getClient()
+            ->setUri($uri)
+            ->setMethod($httpMethod)
+            ->send();
+
+        $this->getProfiler()
+            ->setUrl($uniqueId, $uri)
+            ->setMethod($uniqueId, $httpMethod)
+            ->setParams($uniqueId, $params)
+            ->end($uniqueId);
+
+        return new Response($this->getClient()->getResponse(), $url);
+    }
+}
---
+++ tests/unit/src/Client/ComplexHttpClientTest.php
@@ -0,0 +1,125 @@
+<?php
+
+use G4\Gateway\Client\ComplexHttpClient;
+
+class ComplexHttpClientTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ComplexHttpClient
+     */
+    private $client;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $optionsMock;
+    private $urlValue       = 'http://google.com';
+    private $methodValue    = 'GET';
+    private $paramsValue    = ['id' => 123];
+
+    protected function setUp()
+    {
+        $this->optionsMock = $this->getMockBuilder('\G4\Gateway\Options')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->optionsMock
+            ->expects($this->any())
+            ->method('getHeaders')
+            ->willReturn([
+                'Server'        => 'Apache',
+                'Cache-Control' => 'no-cache',
+                'Content-Type'  => 'application/pdf',
+            ]);
+
+        $this->client = new ComplexHttpClient($this->optionsMock);
+    }
+
+    protected function tearDown()
+    {
+        $this->optionsMock = null;
+        $this->client      = null;
+    }
+
+    public function testGetClient()
+    {
+        $this->optionsMock
+            ->expects($this->once())
+            ->method('getTimeout')
+            ->willReturn(10);
+
+        $this->optionsMock
+            ->expects($this->once())
+            ->method('getSslVerifyPeer')
+            ->willReturn(true);
+
+            $this->optionsMock
+                ->expects($this->once())
+                ->method('getHeaders')
+                ->willReturn([]);
+
+        $this->assertInstanceOf('\Zend\Http\Client', $this->client->getClient());
+    }
+
+    public function testGetProfiler()
+    {
+        $this->assertInstanceOf('\G4\Gateway\Profiler\Ticker', $this->client->getProfiler());
+    }
+
+    public function testSend()
+    {
+        $this->assertInstanceOf('\G4\Gateway\Response', $this->client->send($this->urlMock(), $this->methodMock()));
+    }
+
+    private function urlMock()
+    {
+        $urlMock = $this->getMockBuilder('\G4\Gateway\Url')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $urlMock
+            ->expects($this->once())
+            ->method('getParams')
+            ->willReturn($this->paramsMock());
+
+        $urlMock
+            ->expects($this->once())
+            ->method('getUri')
+            ->willReturn($this->urlValue);
+
+        return $urlMock;
+    }
+
+    private function methodMock()
+    {
+        $methodMock = $this->getMockBuilder('\G4\Gateway\HttpMethod')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $methodMock
+            ->expects($this->once())
+            ->method('isPost')
+            ->willReturn(false);
+
+        $methodMock
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn($this->methodValue);
+
+        return $methodMock;
+    }
+
+    private function paramsMock()
+    {
+        $paramsMock = $this->getMockBuilder('\G4\Gateway\Params')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $paramsMock
+            ->expects($this->once())
+            ->method('toArray')
+            ->willReturn($this->paramsValue);
+
+        return $paramsMock;
+    }
+}
