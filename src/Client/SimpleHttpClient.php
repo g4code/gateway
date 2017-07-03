03e44a6ad0261a2466149f2950082d2d9c07d1ad
Sinisa Lackovic <sinisalackovic@yahoo.com>
2017-07-03 16:30:34 +0200

    Added a way to throw an exception if curl is not successful and a new method for formatting headers

--- src/Client/SimpleHttpClient.php
+++ src/Client/SimpleHttpClient.php
@@ -3,6 +3,7 @@
 namespace G4\Gateway\Client;
 
 use G4\Gateway\Url;
+use G4\Gateway\Options;
 use G4\Gateway\HttpMethod;
 use G4\ValueObject\IntegerNumber;
 use G4\ValueObject\StringLiteral;
@@ -10,9 +11,24 @@ use G4\ValueObject\StringLiteral;
 class SimpleHttpClient implements HttpClientInterface
 {
     /**
+     * @var Options
+     */
+    private $options;
+
+    /**
+     * SimpleHttpClient constructor.
+     * @param $options
+     */
+    public function __construct($options)
+    {
+        $this->options = $options;
+    }
+
+    /**
      * @param Url $url
      * @param HttpMethod $method
      * @return SimpleResponse
+     * @throws \Exception
      */
     public function send(Url $url, HttpMethod $method)
     {
@@ -20,21 +36,41 @@ class SimpleHttpClient implements HttpClientInterface
 
         curl_setopt_array($curl, [
             CURLOPT_URL             => $url->getUri(),
-            CURLOPT_HEADER          => true,
-            CURLOPT_VERBOSE         => 1,
-            CURLINFO_HEADER_OUT     => 1,
+            CURLOPT_CUSTOMREQUEST   => $method,
+            CURLOPT_HTTPHEADER      => $this->getHeaders(),
+            CURLOPT_POSTFIELDS      => json_encode($url->getParams()->toArray()),
+            CURLOPT_VERBOSE         => true,
+            CURLINFO_HEADER_OUT     => true,
             CURLOPT_RETURNTRANSFER  => true,
+
         ]);
 
         $response = curl_exec($curl);
         $error    = curl_error($curl);
+        $code     = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-        list($header, $body) = explode("\r\n\r\n", $response, 2);
-        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        if (!curl_errno($curl)) {
+            return (new SimpleResponse(new StringLiteral($response), new IntegerNumber($code), $url))
+                ->setHeaders($url->getParams()->toArray());
+        }
 
         curl_close($curl);
 
-        return (new SimpleResponse(new StringLiteral($body), new IntegerNumber($code), $url))
-            ->setHeaders(new StringLiteral($header));
+        throw new \Exception($error, 500);
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders()
+    {
+        $headers = [];
+        if(!empty($this->options->getHeaders())) {
+            foreach($this->options->getHeaders() as $key => $value) {
+                $headers[] = $key.': '.$value;
+            }
+        }
+
+        return $headers;
     }
 }
--- tests/unit/src/Client/SimpleHttpClientTest.php
+++ tests/unit/src/Client/SimpleHttpClientTest.php
@@ -2,6 +2,9 @@
 
 use G4\Gateway\Client\SimpleHttpClient;
 
+use G4\Gateway\Params;
+use G4\Gateway\Options;
+
 class SimpleHttpClientTest extends PHPUnit_Framework_TestCase
 {
     /**
@@ -9,16 +12,36 @@ class SimpleHttpClientTest extends PHPUnit_Framework_TestCase
      */
     private $client;
 
+    /**
+     * @var Options
+     */
+    private $optionsMock;
+
+    /**
+     * @var Params
+     */
+    private $paramsMock;
+
+
     private $urlValue = 'https://www.google.com';
 
     protected function setUp()
     {
-        $this->client = new SimpleHttpClient();
+        $this->optionsMock = $this->getMockBuilder('\G4\Gateway\Options')
+        ->disableOriginalConstructor()
+        ->getMock();
+
+        $this->paramsMock = $this->getMockBuilder('\G4\Gateway\Params')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->client = new SimpleHttpClient($this->optionsMock);
     }
 
     protected function tearDown()
     {
-        $this->client = null;
+        $this->client       = null;
+        $this->optionsMock  = null;
     }
 
     public function testSend()
@@ -29,8 +52,20 @@ class SimpleHttpClientTest extends PHPUnit_Framework_TestCase
         );
     }
 
+    public function testSendException()
+    {
+        $this->urlValue = 'tetete.com';
+        $this->setExpectedException('\Exception');
+        $this->client->send($this->urlMock(), $this->methodMock());
+    }
+
     private function urlMock()
     {
+        $this->paramsMock
+            ->expects($this->any())
+            ->method('toArray')
+            ->willReturn([]);
+
         $urlMock = $this->getMockBuilder('\G4\Gateway\Url')
             ->disableOriginalConstructor()
             ->getMock();
@@ -40,7 +75,42 @@ class SimpleHttpClientTest extends PHPUnit_Framework_TestCase
             ->method('getUri')
             ->willReturn($this->urlValue);
 
+        $urlMock
+            ->expects($this->any())
+            ->method('getParams')
+            ->willReturn($this->paramsMock);
+
         return $urlMock;
+    }
+
+    public function testGetHeaders()
+    {
+        $this->optionsMock
+            ->expects($this->any())
+            ->method('getHeaders')
+            ->willReturn([
+                'X-Auth-Email' => 'test@ctest.com',
+                'X-Auth-Key'   => '54d9ab09fc6b97f892f7f687c0c32a9d28448',
+                'Content-Type' => 'application/json'
+            ]);
+
+        $this->assertEquals([
+                'X-Auth-Email: test@ctest.com',
+                'X-Auth-Key: 54d9ab09fc6b97f892f7f687c0c32a9d28448',
+                'Content-Type: application/json',
+            ],
+            $this->client->getHeaders()
+        );
+    }
+
+    public function testGetEmptyHeaders()
+    {
+        $this->optionsMock
+            ->expects($this->any())
+            ->method('getHeaders')
+            ->willReturn([]);
+
+        $this->assertEquals([], $this->client->getHeaders());
     }
 
     private function methodMock()
